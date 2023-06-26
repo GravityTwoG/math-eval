1cb653b7367befc512b7bba874b07e1975aaf602
GravityTwoG <crytekov@gmail.com>
2023-06-26 12:40:21 +0700

    feat: add intermediate representation of expression

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import { Input } from './components/Input';
 import { getLexems, Token } from './lexer';
 import { ParseResult, parse } from './parser';
+import { Command, toPseudoCode } from './toPseudoCode';
 
 export function App() {
   const [expression, setExpression] = useState('');
@@ -13,11 +14,20 @@ export function App() {
     message: '',
     postfix: [],
   });
+  const [intermediate, setIntermediate] = useState<{
+    isValid: boolean;
+    pseudoCode: Command[];
+  }>({
+    isValid: true,
+    pseudoCode: [],
+  });
 
   useEffect(() => {
     const lexems = getLexems(expression);
     setTokens(lexems);
-    setParseResult(parse(lexems));
+    const parseRes = parse(lexems);
+    setParseResult(parseRes);
+    setIntermediate(toPseudoCode(parseRes.postfix));
   }, [expression]);
 
   return (
@@ -47,6 +57,23 @@ export function App() {
 
       <h2>Postfix Notation</h2>
       <p>{parseResult.postfix.join(' ')}</p>
+
+      <h2>PseudoCode</h2>
+      <p>Is valid: {intermediate.isValid.toString()}</p>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <table>
+          <tbody>
+            {intermediate.pseudoCode.map((command, idx) => (
+              <tr key={idx}>
+                <td>{idx + 1}</td>
+                <td>{command.operation}</td>
+                <td>{command.operand1}</td>
+                <td>{command.operand2}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ src/toPseudoCode.ts
@@ -0,0 +1,50 @@
+import { TokenType } from './lexer';
+
+export type Command = {
+  operation: string;
+  operand1: string;
+  operand2: string;
+};
+
+export function toPseudoCode(postfix: string[]) {
+  const pseudoCode: Command[] = [];
+  const opStack: string[] = [];
+
+  for (let i = 0; i < postfix.length; i++) {
+    const word = postfix[i];
+
+    switch (word) {
+      case TokenType.ADD:
+      case TokenType.SUB:
+      case TokenType.MUL:
+      case TokenType.DIV:
+      case TokenType.POW: {
+        if (opStack.length >= 2) {
+          pseudoCode.push({
+            operation: word,
+            operand1: opStack.pop() as string,
+            operand2: opStack.pop() as string,
+          });
+          opStack.push('POP');
+        } else {
+          return {
+            isValid: false,
+            pseudoCode,
+          };
+        }
+        break;
+      }
+      default:
+        if (word.match(/[0-9]+/)) {
+          opStack.push(word);
+        } else {
+          return {
+            isValid: false,
+            pseudoCode,
+          };
+        }
+    }
+  }
+
+  return { isValid: true, pseudoCode };
+}
